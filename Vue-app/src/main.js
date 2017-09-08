eaba9e62d13a080cc31036cb1aca6f71d73ad770
hulei <hulei@nonobank.com>
2017-09-08 13:51:40 +0800

    解决移动端点击延迟

--- Vue-app/src/main.js
+++ Vue-app/src/main.js
@@ -3,12 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 import Mint from 'mint-ui';
 import 'mint-ui/lib/style.css'
 
 // 全局注册mint
 Vue.use(Mint);
 Vue.config.productionTip = false
+
+
+// 解决移动端点击延迟
+fastclick.attach(document.body)
 
 // 导入css
 import 'public/css/index.scss'
