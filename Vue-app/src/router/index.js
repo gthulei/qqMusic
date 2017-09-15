b812772003887f78ea69725c43c8fa9e38724202
hulei <hulei@nonobank.com>
2017-09-15 16:23:59 +0800

    开启路由懒加载

--- Vue-app/src/router/index.js
+++ Vue-app/src/router/index.js
@@ -1,13 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import recommended from 'components/recommended/recommended'
-import singerDes from 'components/singer-des/singer-des'
-import singer from 'components/singer/singer'
-import rankings from 'components/rankings/rankings'
-import topList from 'components/top-list/top-list'
-import search from 'components/search/search'
 Vue.use(Router)
 
+// 开启路由懒加载
+const recommended = (resolve) => {
+  import('components/recommended/recommended').then((module) => {
+    resolve(module)
+  })
+}
+const singerDes = (resolve) => {
+  import('components/singer-des/singer-des').then((module) => {
+    resolve(module)
+  })
+}
+const singer = (resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+}
+const rankings = (resolve) => {
+  import('components/rankings/rankings').then((module) => {
+    resolve(module)
+  })
+}
+const topList = (resolve) => {
+  import('components/top-list/top-list').then((module) => {
+    resolve(module)
+  })
+}
+const search = (resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+}
 export default new Router({
   routes: [
     {
